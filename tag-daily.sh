cfc42f19d25f8e483455f7439b33ab52c1519e74
Johan Gall <johan.gall@gmail.com>
2019-09-24 19:04:19 +0900

    fix

--- tag-daily.sh
+++ tag-daily.sh
@@ -2,7 +2,11 @@
 
 set -euxo pipefail
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 DATE=$(date +"%d-%m-%Y")
+
+pushd "$DIR"
 
 cat <<EOF > ./DATE
 daily tag for autobuild in docker hub:
@@ -12,3 +16,5 @@ git add DATE
 git commit -am "daily"
 git tag "$DATE"
 git push origin --tags
+
+popd
