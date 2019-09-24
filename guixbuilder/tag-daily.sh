952b1ad5c52bd4da816666386b7dc1c2a987c4c6
Johan Gall <johan.gall@gmail.com>
2019-09-24 13:22:25 +0900

    fix

--- guixbuilder/tag-daily.sh
+++ guixbuilder/tag-daily.sh
@@ -4,7 +4,10 @@ set -euxo pipefail
 
 DATE=$(date +"%d-%m-%Y")
 
-cat "$DATE" > ./DATE
+cat ./DATE <<EOF
+daily tag for autobuild in docker hub:
+$DATE
+EOF
 git add DATE
 git commit -am "daily"
 git tag "$DATE"
