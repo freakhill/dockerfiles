be269f99eb5af8579b9fe1e7fc99e4220652492e
Johan Gall <johan.gall@gmail.com>
2020-03-05 18:58:37 +0900

    added stuff

--- basedev/install/stuff.sh
+++ basedev/install/stuff.sh
@@ -8,3 +8,6 @@ pip install --user awscli
 
 # rust implementation of minisign, a tool to sign and verify files
 #cargo install rsign
+
+# CSV,JSON,Redis,Mysql,PostgreSQL querying with SQL
+GO111MODULE=on go get -u github.com/cube2222/octosql/cmd/octosql
