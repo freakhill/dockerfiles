241760c453907e67285bd6269e00ba73acb03418
Johan Gall <johan.gall@gmail.com>
2020-05-28 15:40:38 +0900

    try

--- basedev/install/rust-root.sh
+++ basedev/install/rust-root.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 # for ripgrep-all
-apt -y install build-essential pandoc poppler-utils ffmpeg
+apt-get -y install build-essential pandoc poppler-utils ffmpeg
 # for zola
-apt -y install libssl-dev
+apt-get -y install libssl-dev
 # for mdcat (for onig-sys crate})
 apt-get -y install llvm clang
 # for nu
