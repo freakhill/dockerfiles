c3d8e1d465f7c2a27de07a80cbc98f01b05aab94
Johan Gall <johan.gall@gmail.com>
2020-05-28 14:33:03 +0900

    try snap

--- basedev/install/emacs-root.sh
+++ basedev/install/emacs-root.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-add-apt-repository ppa:kelleyk/emacs
-apt-get update -y
-apt-get install -y emacs26
+#add-apt-repository ppa:kelleyk/emacs
+#apt-get update -y
+#apt-get install -y emacs26
+snap install emacs --beta --classic
 
