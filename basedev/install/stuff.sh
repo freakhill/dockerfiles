648449840bcfbea3b1d5c9eb59fe25e93dcecca8
jojo <johan.gall@gmail.com>
2020-11-20 09:52:21 +0900

    fix

--- basedev/install/stuff.sh
+++ basedev/install/stuff.sh
@@ -9,7 +9,8 @@ pip install --user awscli
 # rust implementation of minisign, a tool to sign and verify files
 #cargo install rsign
 
+# CSV,JSON,Redis,Mysql,PostgreSQL querying with SQL
+wget https://github.com/cube2222/octosql/releases/download/v0.3.0/octosql-linux
+mv octosql-linux ~/.local/bin/octosql
 # websocket queries
 go get -u github.com/hashrocket/ws
-# CSV,JSON,Redis,Mysql,PostgreSQL querying with SQL
-GO111MODULE=on go get -u github.com/cube2222/octosql/cmd/octosql
