e932b1d83ee465577f4b60af7c67699aafadf043
jojo <johan.gall@gmail.com>
2020-12-17 01:13:39 +0900

    try

--- basedev/install/clojure-root.sh
+++ basedev/install/clojure-root.sh
@@ -1,15 +1,12 @@
 #!/usr/bin/env bash
 
 apt-get -y install nodejs npm
-# plank
-add-apt-repository -y ppa:mfikes/planck
-apt-get -y update
-# no plank available now for some reason
-#apt-get -y install planck
 
-curl -O https://download.clojure.org/install/linux-install-1.10.1.502.sh
-chmod +x linux-install-1.10.1.502.sh
-./linux-install-1.10.1.502.sh
+curl -O https://download.clojure.org/install/linux-install-1.10.1.754.sh
+chmod +x linux-install-1.10.1.754.sh
+./linux-install-1.10.1.754.sh
 
 npm install -g shadow-cljs
-npm install -g lumo-cljs
+# install babashka
+bash <(curl -s https://raw.githubusercontent.com/borkdude/babashka/master/install)
+
