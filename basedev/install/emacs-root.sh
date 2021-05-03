52f61f91f99a2d8a69942f85319b3a4510daf67c
Johan Gall <johan.gall@gmail.com>
2021-05-03 09:34:05 +0900

    compile emacs

--- basedev/install/emacs-root.sh
+++ basedev/install/emacs-root.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 
-#add-apt-repository ppa:kelleyk/emacs
+#add-apt-repository ppa:ubuntu-elisp/ppa
 #apt-get update -y
-#apt-get install -y emacs26
-add-apt-repository ppa:ubuntu-elisp/ppa
-apt-get update -y
-export DEBIAN_FRONTEND=noninteractive
-apt-get install -y emacs-snapshot
+#export DEBIAN_FRONTEND=noninteractive
+#apt-get install -y emacs-snapshot
+
+apt-get install -y autoconf libgccjit-10-dev libxpm-dev libgif-dev libjansson-dev libgnutls28-dev libharfbuzz-dev libmagickwand-dev texinfo
+pushd ~
+git clone git://git.savannah.gnu.org/emacs.git
+cd emacs
+./autogen.sh
+./configure --with-native-compilation --with-json --with-imagemagick --with-x-toolkit=no
+make -j$(nproc)
+make install
+popd
+rm -fr ~/emacs
+
