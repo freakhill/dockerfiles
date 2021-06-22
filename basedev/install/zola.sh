69513a4a8d1c8735fcd40941c726b97c24bd129c
Johan Gall <johan.gall@gmail.com>
2021-06-22 21:29:23 +0900

    update zola build

--- basedev/install/zola.sh
+++ basedev/install/zola.sh
@@ -4,7 +4,7 @@ set -e
 
 source $HOME/.bash_profile
 cd /install
-git clone git@github.com:getzola/zola
+git clone --depth 1 --branch v0.13.0 git@github.com:getzola/zola
 cd ./zola
 cargo build --release
 cp ./target/release/zola $HOME/.local/bin/
