0af865a18be1883d8c7f057995eeea406400a5a3
Johan Gall <johan.gall@gmail.com>
2021-06-22 21:38:34 +0900

    try

--- basedev/install/zola.sh
+++ basedev/install/zola.sh
@@ -6,7 +6,7 @@ source $HOME/.bash_profile
 cd /install
 git clone --depth 1 --branch v0.13.0 git@github.com:getzola/zola
 cd ./zola
-cat <<EOF >./Cargo.toml
+cat <<EOF >>Cargo.toml
 
 [patch.crates-io]
 lexical-core = {git = 'https://github.com/Gelbpunkt/rust-lexical', branch = 'fix-warnings-and-update-deps'}
